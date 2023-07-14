a3a489910d068592436d76a8c6758bc56a7178d0
phatnguyen144 <74170296+phatnguyen144@users.noreply.github.com>
2023-07-14 09:32:50 +0700

    add 3 project

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'clustersgp.qdufowt.mongodb.net',
+    DB_USER: 'phatnguyen1442000',
+    DB_PASS: '0922313171A',
+    DB_DATABASE: 'shoppingonline',
+    EMAIL_USER: 'phatnguyen1442000@gmail.com', // Microsoft mail service
+    EMAIL_PASS: 'dfwxydgiomqtjydc',
+    JWT_SECRET: '123aaa123',
+    JWT_EXPIRES: '864000000', // in milliseconds
+  };
+  module.exports = MyConstants;
